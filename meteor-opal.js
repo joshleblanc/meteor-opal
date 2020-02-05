5e2ce1abdac4f4200607fe8097b009f6bdeff7e5
Joshua LeBlanc <jleblanc@omics.com>
2020-02-05 13:42:06 -0400

    initial build attempt

--- meteor-opal.js
+++ meteor-opal.js
@@ -1,5 +1,26 @@
-// Write your package code here!
+import { Builder } from 'opal-compiler';
 
-// Variables exported by this module can be imported by other packages and
-// applications. See meteor-opal-tests.js for an example of importing.
 export const name = 'meteor-opal';
+const builder = Builder.create();
+
+const handler = compileStep => {
+  const source = compileStep.read().toString('utf8');
+  const outputFile = compileStep.inputPath + ".js";
+
+  try {
+    builder.buildString(source).toString();
+    compileStep.addJavaScript({
+      path: outputFile,
+      sourcePath: compileStep.inputPath,
+      data: output,
+      bare: compileStep.fileOptions.bare
+    });
+  } catch(e) {
+    throw new Error(
+      compileStep.inputPath + ": " + (e.location ? (e.location.first_line + ": ") : ' ') + e.message
+    );
+  }
+}
+
+Plugin.registerSourceHandler("rb", handler);
+Plugin.registerSourceHandler("js.rb", handler);
--- package.js
+++ package.js
@@ -1,24 +1,36 @@
 Package.describe({
-  name: 'meteor-opal',
+  name: 'cereal:meteor-opal',
   version: '0.0.1',
   // Brief, one-line summary of the package.
   summary: '',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/joshleblanc/meteor-opal',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
+
+Package.registerBuildPlugin({
+  name: "compileOpal",
+  use: [],
+  sources: [
+    'meteor-opal.js'
+  ],
+});
+
+
 Package.onUse(function(api) {
   api.versionsFrom('1.9');
-  api.use('ecmascript');
   api.mainModule('meteor-opal.js');
 });
 
 Package.onTest(function(api) {
-  api.use('ecmascript');
   api.use('tinytest');
   api.use('meteor-opal');
   api.mainModule('meteor-opal-tests.js');
+});
+
+Npm.depends({
+  "opal-compiler": "^1.0.15"
 });
