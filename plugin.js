cb7f87163d66ef4e302a833abb284306558e61a4
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-02-05 22:03:53 -0400

    fix: give up on sourcemap

--- plugin.js
+++ plugin.js
@@ -3,16 +3,15 @@ const Builder = Npm.require('opal-compiler').Builder;
 
 class OpalCompiler {
   processFilesForTarget(files) {
-
     files.forEach((file) => {
       const builder = Builder.create();
-      const output = builder.buildString(file.getContentsAsString()).toString();
-      console.log(Object.getOwnPropertyNames(builder.getSourceMap().$map().to_n()));
+      const output = builder.buildString(file.getContentsAsString()).toString('utf8');
+      const map = JSON.parse(builder.getSourceMap().$map().$to_s().replace(/\=\>/g, ":"));
+      map.sections[0].map.sources = [file.getDisplayPath()];
       file.addJavaScript({
         data: output,
-        sourceMap: builder.getSourceMap().$map(),
-        path: `${file.getPathInPackage()}.js`
-      })
+        path: `${file.getDisplayPath()}.js`
+      });
     })
   }
 }
